aefe1d679e547cea3d4def4eedaaac6e1458214a
JuanPmf <juan.murielfranco@gmail.com>
2023-07-23 12:35:57 -0500

    version 1

---
+++ CAMP_Return.py
@@ -0,0 +1,9 @@
+# Import libraries
+
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+
+st.set_page_config(page_title = "CAMP: Capital Asset Pricing Model",
+    page_icon = "Chart_with:upwards_trend",
+    layout = 'wide')
