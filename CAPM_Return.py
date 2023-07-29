1cc63ea559cdabfccfecf29397ccb6e1808955ed
JuanPmf <juan.murielfranco@gmail.com>
2023-07-29 17:32:59 -0500

    version 5: This version is working well

--- CAPM_Return.py
+++ CAPM_Return.py
@@ -5,13 +5,16 @@ import pandas as pd
 import yfinance as yf
 import pandas_datareader.data as pdr
 import datetime as dt
+# To solve string indeces error  
+yf.pdr_override()
 
+#Get NASDAQ tickers from local CSV
+
+tickers = pd.read_csv('D:/Projects/CAMP-WebApp/NASDAQ.csv')
 
 st.set_page_config(page_title = "CAPM",
     page_icon = "Chart_with:upwards_trend",
     layout = 'wide')
-
-#Run the app using terminal in the folder: py -m streamlit run CAMP_return.py
 
 st.title("CAMP: Capital Asset Pricing Model")
 
@@ -20,7 +23,7 @@ st.title("CAMP: Capital Asset Pricing Model")
 col1, col2 = st.columns([1,1])
 
 with col1:
-    stocks_list = st.multiselect("Choose 4 stocks", ("TSLA","APPL", "NFLX", "MSFT", "MGM", "NVDA","GOOGL"), ["TSLA","APPL", "NFLX"])
+    stocks_list = st.multiselect("Choose 4 stocks", (tickers['Symbol'].tolist()), ["TSLA"])
 with col2:
     year = st.number_input("Number of Years", 1, 10)
 
@@ -33,3 +36,6 @@ end = dt.date.today()
 sp500 = pdr.get_data_yahoo(stocks_list, start, end)
 
 sp500
+
+
+#Run from anaconda promt: streamlit run D:\Projects\CAMP-WebApp\CAPM_Return.py
