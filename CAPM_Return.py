02d24146744e0417d573349a3bc05bc599a9805a
JuanPmf <juan.murielfranco@gmail.com>
2023-08-13 08:08:40 -0500

    Version 9: Graphics

--- CAPM_Return.py
+++ CAPM_Return.py
@@ -5,7 +5,7 @@ import pandas as pd
 import yfinance as yf
 import pandas_datareader.data as pdr
 import datetime as dt
-
+import CAPM_Functions
 # To solve string indeces error
 yf.pdr_override()
 
@@ -26,11 +26,11 @@ col1, col2 = st.columns([1,1])
 with col1:
     stocks_list = st.multiselect("Choose 4 stocks", (tickers['Symbol'].tolist()), ["TSLA"])
 with col2:
-    year = st.number_input("Number of Years", 1, 10)
+    year = st.number_input("Number of Years", 1, 5)
 
 # Downloading NASDAQCOMP data from https://fred.stlouisfed.org/series/NASDAQCOM
 
-url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=718&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=NASDAQCOM&scale=left&cosd=2018-07-27&coed=2023-07-27&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily%2C%20Close&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2023-07-30&revision_date=2023-07-30&nd=1971-02-05'
+url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=NASDAQCOM&scale=left&cosd=2018-08-10&coed=2023-08-10&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily%2C%20Close&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2023-08-12&revision_date=2023-08-12&nd=1971-02-05'
 nasdaq = pd.read_csv(url)
 nasdaq.columns = ['Date', 'NSDQ']
 nasdaq['Date'] = nasdaq['Date'].astype('datetime64[ns]')
@@ -55,6 +55,20 @@ stocks_df['Date'] = pd.to_datetime(stocks_df['Date'])
 # Mergin NASDAQ and Stocks data
 stocks_df = pd.merge(stocks_df, nasdaq, on= 'Date', how = 'inner')
 
-stocks_df
+# Split head and tail in 2 columns
+
+col1, col2 = st.columns([1,1])
+with col1:
+    st.markdown("### Dataframe Head")
+    st.dataframe(stocks_df.head(), use_container_width= True)
+with col2:
+    st.markdown("### Dataframe Tail")
+    st.dataframe(stocks_df.tail(), use_container_width= True)
+
+
+col1, col2 = st.columns([1,1])
+with col1:
+    st.markdown("### Price of all the stocks")
+    st.plotly_chart(CAPM_Functions.interactive_plot(stocks_df))
 
 #Run from anaconda promt: streamlit run D:\Projects\CAMP-WebApp\CAPM_Return.py
